9cb41ee8465755cdae4d1b6686f50f959f10f6eb
ushishu <shuzo.1128@gmail.com>
2018-10-28 17:13:23 +0900

    add line massage

--- main.py
+++ main.py
@@ -4,16 +4,19 @@
 import json
 import config
 from requests_oauthlib import OAuth1Session
+import requests
 
-CK = config.CONSUMER_KEY
-CS = config.CONSUMER_SECRET
-AT = config.ACCESS_TOKEN
-ATS = config.ACCESS_TOKEN_SECRET
+TCK = config.TWI_CONSUMER_KEY
+TCS = config.TWI_CONSUMER_SECRET
+TAT = config.TWI_ACCESS_TOKEN
+TATS = config.TWI_ACCESS_TOKEN_SECRET
+LT = config.LINE_TOKEN
 
-twitter_oauth = OAuth1Session(CK, CS, AT, ATS)
+twitter_oauth = OAuth1Session(TCK, TCS, TAT, TATS)
 
 get_timeline_url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
 search_tweet_word_url = "https://api.twitter.com/1.1/search/tweets.json?"
+post_line_url = "https://notify-api.line.me/api/notify"
 
 def get_timeline(oauth, url):
     responce = oauth.get(url)
@@ -26,8 +29,19 @@ def get_searching_tweet_word(oauth, url, word):
         "result_type": "recent",
         "count": "5"
     }
-    responce = oauth.get(url, params)
-    return json.loads(responce.text)
+    response = oauth.get(url, params = params)
+    return json.loads(response.text)
+
+def post_line_message(token, url, word):
+    header = {
+        "Authorization": "Bearer " + token
+    }
+    payload = {
+        "message": word
+    }
+
+    response = requests.post(url, data = payload, headers = header)
+    print(response)
 
 
 timeline = get_timeline(twitter_oauth, get_timeline_url)
@@ -36,3 +50,5 @@ print("timeline:", timeline)
 word = "ushi"
 search = get_searching_tweet_word(twitter_oauth, search_tweet_word_url, word)
 print("search: ", search)
+
+post_line_message(LT, post_line_url, "Succeed")
